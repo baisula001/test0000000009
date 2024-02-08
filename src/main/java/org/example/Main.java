20da02bdf767610d79860792fa94b8cfc30d316f
3575205331a@gmail.com <3575205331a@gmail.com>
2024-02-08 16:17:48 +0100

    first try

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * @Auther: willson2008
+ * @Date: 08/02/2024 - 02 - 08 - 16:14
+ * @Description: org.example
+ * @Version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+    }
+}
