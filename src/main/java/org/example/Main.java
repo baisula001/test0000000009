2c36c68df5d3427c390bea676ab8c869748c69e8
3575205331a@gmail.com <3575205331a@gmail.com>
2024-02-08 16:21:30 +0100

    third try

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,7 +12,11 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
-        System.out.println("Hello world!");System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
         System.out.println("Hello world!");
 
 
